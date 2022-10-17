46f1113832c6d06de9855a3a11e8965e9405244f
JaySharma1048576 <56032753+JaySharma1048576@users.noreply.github.com>
2022-10-17 19:07:36 +0530

    Improved graphics and transitions of the game (#3)

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import pygame
 if __name__ == '__main__':
     b = Board()
     action = 0
-    thr = 0
-    mx = 5
     print("Game Running ...")
     while True:
         b.draw()
@@ -20,7 +18,6 @@ if __name__ == '__main__':
                     action = 3
                 elif event.key == pygame.K_DOWN:
                     action = 4
-                thr = 4
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT and action == 1:
                     action = 0
@@ -33,13 +30,6 @@ if __name__ == '__main__':
             elif event.type == pygame.QUIT:
                 b.close()
                 sys.exit()
-        if thr == 0:
-            if b.player.move(0) != 0:
-                b.close()
-                break
-        else:
-            if b.player.move(action) != 0:
-                b.close()
-                break
-        thr = (thr + mx - 1) % mx
-        # time.sleep(0.1)
+        if b.engine.move(action) != 0:
+            b.close()
+            break
