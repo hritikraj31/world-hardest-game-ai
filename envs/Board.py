42b7fd4d41a9bfcfcaab2b72cec0314ee2f90375
Jay Sharma <jay.sharma25102001@gmail.com>
2022-10-17 16:44:42 +0530

    Improved graphics and transitions of the game

--- envs/Board.py
+++ envs/Board.py
@@ -1,63 +1,83 @@
-from envs.Player import Player
+from envs.Engine import Engine
 import pygame
 from pygame import gfxdraw
+from enum import Enum
+from typing import Tuple
 
 
 class Board:
     def __init__(self):
-        self.rows = 15
-        self.cols = 20
-        self.heightPixel = 20
-        self.widthPixel = 20
-        # self.height = 200
-        # self.width = 200
-        self.height = self.rows * self.heightPixel
-        self.width = self.cols * self.widthPixel
-        self.grid = [[0 for j in range(self.cols)] for i in range(self.rows)]
+        self.height = 200
+        self.width = 200
+        self.cell_size = 20
+        self.ballSpeed = 4
+        self.playerSpeed = 3
         self.balls = []
+        self.walls = []
+        self.player = [0, 0, 0, 0]
+        self.goal = [0, 0, 0, 0]
         self.read_map()
         self.screen = None
         self.surf = None
         self.clock = None
+        self.engine = Engine(self)
 
     def draw(self):
-        colors = [(255, 255, 255), (128, 128, 128), (0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0)]
+        class Colors(Enum):
+            RED = (1, (255, 0, 0))
+            GREEN = (2, (50, 255, 50))
+            BLUE = (3, (0, 0, 255))
+            GRAY = (4, (191, 191, 191))
+            BLACK = (5, (0, 0, 0))
+            WHITE = (6, (255, 255, 255))
+
+            def __str__(self) -> str:
+                return self.name.lower()
+
+            def num(self) -> int:
+                return self.value[0]
+
+            def rgb(self) -> Tuple:
+                return self.value[1]
         if self.screen is None:
             pygame.init()
             pygame.display.init()
             self.screen = pygame.display.set_mode((self.width, self.height))
         if self.clock is None:
             self.clock = pygame.time.Clock()
-        self.surf  = pygame.Surface((self.width, self.height))
+        self.surf = pygame.Surface((self.width, self.height))
         self.surf.fill((255, 255, 255))
-        for i in range(self.rows):
-            for j in range(self.cols):
-                x = j * self.widthPixel
-                y = i * self.heightPixel
-                if self.grid[i][j]>3:
-                    gfxdraw.filled_circle(self.surf, x + self.widthPixel//2, y + self.heightPixel//2, self.widthPixel//2-1, colors[self.grid[i][j]])
-                else:
-                    gfxdraw.box(self.surf, (x, y, self.widthPixel, self.heightPixel), colors[self.grid[i][j]])
+        for i in range(self.width // self.cell_size):
+            for j in range(self.height // self.cell_size):
+                if i % 2 == j % 2:
+                    pygame.gfxdraw.box(self.surf, (i*self.cell_size, j*self.cell_size, self.cell_size, self.cell_size), Colors.GRAY.rgb())
+        for wall in self.walls:
+            pygame.gfxdraw.box(self.surf, (wall[0], wall[1], wall[2], wall[3]), Colors.BLACK.rgb())
+        pygame.gfxdraw.box(self.surf, (self.goal[0], self.goal[1], self.goal[2], self.goal[3]), Colors.GREEN.rgb())
+        for ball in self.balls:
+            pygame.gfxdraw.filled_circle(self.surf, ball[0], ball[1], ball[2], Colors.RED.rgb())
+        pygame.gfxdraw.box(self.surf, (self.player[0], self.player[1], self.player[2], self.player[3]), Colors.BLUE.rgb())
         self.screen.blit(self.surf, (0, 0))
         pygame.event.pump()
-        self.clock.tick(20)
+        self.clock.tick(50)
         pygame.display.flip()
 
     def read_map(self):
         filename = 'envs/GameMap'
         file = open(filename, 'r')
-        i = 0
         for line in file:
-            j = 0
-            for x in line.split(' '):
-                x = int(x)
-                self.grid[i][j] = x
-                if x == 2:
-                    self.player = Player(i, j, self)
-                elif x > 3:
-                    self.balls.append([i, j, x - 3])
-                j += 1
-            i += 1
+            data = line.split(' ')
+            if data[0] == 'P':
+                self.player = [int(data[1]), int(data[2]), int(data[3]), int(data[4])]
+            elif data[0] == 'W':
+                self.walls.append([int(data[1]), int(data[2]), int(data[3]), int(data[4])])
+            elif data[0] == 'G':
+                self.goal = [int(data[1]), int(data[2]), int(data[3]), int(data[4])]
+            elif data[0] == 'B':
+                self.balls.append([int(data[1]), int(data[2]), int(data[3]), int(data[4])])
+            else:
+                self.width = int(data[0])
+                self.height = int(data[1])
 
     def close(self):
         pygame.display.quit()
---
+++ envs/Engine.py
@@ -0,0 +1,99 @@
+import time
+
+
+class Engine:
+    def __init__(self, board):
+        self.board = board
+        # 0 - stat 1 - right 2 - up 3 - left 4 - down
+
+    def intersectionRect(self, r1, r2):
+        lx = max(r1[0], r2[0])
+        rx = min(r1[0]+r1[2], r2[0]+r2[2])
+        ly = max(r1[1], r2[1])
+        ry = min(r1[1]+r1[3], r2[1]+r2[3])
+        if (lx < rx) and (ly < ry):
+            return (rx - lx) * (ry - ly)
+        else:
+            return 0
+
+    def intersectionBW(self, ball, wall):
+        if ball[3] == 1 and self.intersectionRect([ball[0]-ball[2], ball[1]-ball[2], 2*ball[2], 2*ball[2]], wall) > 0:
+            return max(0, (ball[0]+ball[2])-(wall[0]))
+        elif ball[3] == 2 and self.intersectionRect([ball[0]-ball[2], ball[1]-ball[2], 2*ball[2], 2*ball[2]], wall) > 0:
+            return max(0, (wall[1]+wall[3])-(ball[1]-ball[2]))
+        elif ball[3] == 3 and self.intersectionRect([ball[0]-ball[2], ball[1]-ball[2], 2*ball[2], 2*ball[2]], wall) > 0:
+            return max(0, (wall[0]+wall[2])-(ball[0]-ball[2]))
+        elif ball[3] == 4 and self.intersectionRect([ball[0]-ball[2], ball[1]-ball[2], 2*ball[2], 2*ball[2]], wall) > 0:
+            return max(0, (ball[1]+ball[2])-(wall[1]))
+        return 0
+
+    def intersectionPW(self, player, wall):
+        return self.intersectionRect(player, wall)
+
+    def intersectionPB(self, player, ball):
+        return self.intersectionRect(player, [ball[0]-ball[2], ball[1]-ball[2], 2*ball[2], 2*ball[2]])
+
+    def intersectionPG(self, player, goal):
+        return self.intersectionRect(player, goal)
+
+    def move(self, action):
+        [orig_x, orig_y] = [self.board.player[0], self.board.player[1]]
+        if action == 1:
+            self.board.player[0] += self.board.playerSpeed
+        elif action == 2:
+            self.board.player[1] -= self.board.playerSpeed
+        elif action == 3:
+            self.board.player[0] -= self.board.playerSpeed
+        elif action == 4:
+            self.board.player[1] += self.board.playerSpeed
+        original_balls = [x.copy() for x in self.board.balls]
+        for ball in self.board.balls:
+            if ball[3] == 1:
+                ball[0] += self.board.ballSpeed
+                for wall in self.board.walls:
+                    overlap = self.intersectionBW(ball, wall)
+                    if overlap > 0:
+                        ball[0] -= 2*overlap
+                        ball[3] = 3
+                        break
+            elif ball[3] == 2:
+                ball[1] -= self.board.ballSpeed
+                for wall in self.board.walls:
+                    overlap = self.intersectionBW(ball, wall)
+                    if overlap > 0:
+                        ball[1] += 2*overlap
+                        ball[3] = 4
+                        break
+            elif ball[3] == 3:
+                ball[0] -= self.board.ballSpeed
+                for wall in self.board.walls:
+                    overlap = self.intersectionBW(ball, wall)
+                    if overlap > 0:
+                        ball[0] += 2*overlap
+                        ball[3] = 1
+                        break
+            elif ball[3] == 4:
+                ball[1] += self.board.ballSpeed
+                for wall in self.board.walls:
+                    overlap = self.intersectionBW(ball, wall)
+                    if overlap > 0:
+                        ball[1] -= 2*overlap
+                        ball[3] = 2
+                        break
+
+        for wall in self.board.walls:
+            overlap = self.intersectionPW(self.board.player, wall)
+            if overlap > 0:
+                self.board.player[0] = orig_x
+                self.board.player[1] = orig_y
+        reached_goal = self.intersectionPG(self.board.player, self.board.goal)
+        if reached_goal == self.board.player[2]*self.board.player[3]:
+            self.board.draw()
+            time.sleep(0.5)
+            return 1
+        for idx, ball in enumerate(self.board.balls):
+            if self.intersectionPB(self.board.player, ball) > 0:
+                self.board.draw()
+                time.sleep(0.5)
+                return -1
+        return 0
